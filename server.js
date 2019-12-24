213e9de1eae5e6b27f9671aee4484c056e27a3f6
smallcoderofme <svensama@outlook.com>
2019-12-24 17:02:47 +0800

    enter content

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path');
 const fs = require('fs');
 const uuidv4 = require('uuid/v4');
 const config = require('./config');
+const tpl = require('./template');
 
 app.use(express.static(__dirname+'/'));
 // app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')));
@@ -22,27 +23,22 @@ var data = [
     {
         "name":"科学馆篇",
         "uuid":"35efe1dc-741a-4e67-a55d-ec9a848d9e1b",
-        "url":"sicence",
         "sub": [
             {
                 "name":"生命科学馆介绍",
                 "uuid":"2257b8d4-a2f3-4a51-a1f0-75176bb46f1a",
-                "url":"sicence"
             },
             {
                 "name":"生命之初",
                 "uuid":"4abc7321-9a0b-49c1-b9df-51f7279dcc71",
-                "url":"sicence"
             },
             {
                 "name":"生命之美",
                 "uuid":"981989d7-2357-42ed-a3d6-a513557f114b",
-                "url":"sicence"
             },
             {
                 "name":"生命之本",
                 "uuid":"ea52952c-b729-4c44-b1a3-cb1a375a7099",
-                "url":"sicence"
             }
         ]
     },
@@ -54,66 +50,54 @@ var data = [
             {
                 "name":"解剖学名人名言",
                 "uuid":"62424643-541a-47cf-86a5-b1a76f00aca0",
-                "url":"anatomy"
             },
             {
                 "name":"解剖学发展历程",
                 "uuid":"cb4b1548-bbb5-403b-9610-497fe7632ed7",
-                "url":"anatomy"
             },
             {
                 "name":"解剖学人体九大系统",
                 "uuid":"66b17f54-e2dc-4517-a8c1-04ab01c9a6f0",
-                "url":"anatomy"
             }
         ]
     },
     {
         "name":"人体生命篇",
         "uuid":"13d3a4e8-e9c2-4ab5-bf5c-124ca688dc27",
-        "url":"human",
         "sub": [
             {
                 "name":"大脑的奥义",
                 "uuid":"9e630e24-f1a7-4544-ab4d-ab788814548b",
-                "url":"human"
             },
             {
                 "name":"基因工程与人类健康",
                 "uuid":"756108bf-33cc-4467-aa08-b4f32ae8544a",
-                "url":"human"
             },
             {
                 "name":"DNA双螺旋结构的发现",
                 "uuid":"ed51c7ce-a96e-463e-ad63-4bae4f893168",
-                "url":"human"
             }
         ]
     },
     {
         "name":"医学院系篇",
         "uuid":"4a73b2a5-670f-41f8-8f23-cba3c8839167",
-        "url":"medical",
         "sub": [
             {
                 "name":"学院风采",
                 "uuid":"ed51c7ce-a96e-463e-ad63-4bae4f893168",
-                "url":"medical"
             },
             {
                 "name":"院系介绍",
                 "uuid":"8205f62b-a9ad-4f75-8418-f606a0b12e79",
-                "url":"medical"
             },
             {
                 "name":"名师风采",
                 "uuid":"5e913398-f121-460b-92d8-93f422737a12",
-                "url":"medical"
             },
             {
                 "name":"实验室简介",
                 "uuid":"b20cc372-0041-4e10-94a2-26681c697a9e",
-                "url":"medical"
             }
         ]
     }
@@ -121,6 +105,10 @@ var data = [
 
 app.get('/', (req, res) => {
    res.sendFile(__dirname + '/src/index.html');
+});
+
+app.get('/menu', (req, res) => {
+    res.sendFile(__dirname + '/src/index.html');
 });
 
 app.get('/list', (req, res) => {
@@ -168,7 +156,7 @@ app.post('/modify_menu', function (req, res) {
 
 app.post('/delete_menu', function (req, res) {
     const len = data.length;
-    const id = req.body.uuid;
+    const id = req.body.pid;
     for (let i =0;i<len;i++) {
         if (data[i].uuid == id) {
             data.splice(i,1);
@@ -200,14 +188,53 @@ app.get('/edit', function(req, res) {
 
 });
 
-app.post('/post_sub_menu', function (req, res) {
-    let new_menu = req.body.data;
-    data.push({
-        name: new_menu,
-        uuid: uuidv4(),
-        sub: []
+app.get('/create_content', function (req, res) {
+    if(verify_token(req.query.token)){
+        res.sendFile(__dirname + '/src/create.html');
+    } else {
+        res.sendFile(__dirname + '/src/login.html');
+    }
+});
+
+app.post('/save_content', function(req, res) {
+    const uuid = uuidv4();
+    const file_name = 'pages/'+ uuid + '.html';
+    const content_html = req.body.data;
+    // fs.readFile('config.json', 'utf8', (err, data) => {
+    //     if (err) {
+    //         res.send({
+    //             status: 'error',
+    //             data: "Error: config.js can't read or not exist."
+    //         });
+    //         throw err;
+    //     }
+        const len = data.length;
+        let parent;
+        console.log(req.body.pid, data);
+        for (let i=0;i<len;i++) {
+            if ( data[i].uuid == req.body.pid ) {
+                parent = data[i];
+                break;
+            }
+        }
+        console.log('parent',parent);
+        parent.sub[parent.sub.length] = {
+            name: req.body.title,
+            uuid: uuid,
+        };
+
+    // });
+    fs.writeFile(file_name, tpl.tmplate_0+content_html+tpl.tmplate_1, (err) => {
+        if (err) throw err;
+        console.log('The html file has been saved!');
+        res.send({status:'ok'});
     });
-    res.send({status: 'ok', data:data});
+});
+
+
+app.get('/menu/:uuid/content/:cid', function(req, res) {
+    // res.send({status:'ok', data:{pid: req.params.uuid, cid:req.params.cid}});
+    res.sendFile(__dirname + '/pages/' + req.params.cid + '.html');
 });
 
 app.listen(3000, function () {
---
+++ template.js
@@ -0,0 +1,75 @@
+const tpl_next = `</div></div></div></div><div class="col-md-2"></div></div></body></html>`;
+const tpl_pre = `<!DOCTYPE html><html><head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0">
+  <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+
+  <!-- Include Font Awesome. -->
+  <link href="/bower_components/font-awesome/css/fontawesome.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Include Froala Editor styles -->
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/froala_editor.min.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/froala_style.min.css">
+
+  <!-- Include Froala Editor Plugins styles -->
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/char_counter.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/code_view.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/colors.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/emoticons.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/file.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/fullscreen.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/image_manager.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/image.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/line_breaker.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/table.css">
+  <link rel="stylesheet" href="/bower_components/froala-wysiwyg-editor/css/plugins/video.css">
+  <link rel="stylesheet" type="text/css" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+
+  <!-- Include Froala Editor -->
+  <script src="/bower_components/froala-wysiwyg-editor/js/froala_editor.min.js"></script>
+
+  <!-- Include Froala Editor Plugins -->
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/align.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/char_counter.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/code_beautifier.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/code_view.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/colors.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/emoticons.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/entities.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/file.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/font_family.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/font_size.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/fullscreen.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/image.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/image_manager.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/inline_style.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/line_breaker.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/link.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/lists.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/paragraph_format.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/paragraph_style.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/quote.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/save.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/table.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/video.min.js"></script>
+  <script src="/bower_components/froala-wysiwyg-editor/js/plugins/edit_in_popup.min.js"></script>
+  <!-- End Froala -->
+  <link rel="stylesheet" href="../../../style.css">
+</head>
+<body>
+  <script type="text/javascript">
+      function list() {
+        window.location.href = location.origin;
+      }
+  </script>
+  <div class="row">
+    <div class="col-md-2">
+      <button type="button" data-toggle="tooltip" 
+            title="返回列表"  class="btn btn-outline-success btn-sm" onclick="list();"><</button></div>
+    <div class="col-md-8">
+      <div class="fr-box fr-basic fr-top" role="application">
+        <div class="fr-wrapper" style="border: 1px solid #CCCCCC;" dir="auto">
+        <div class="fr-element fr-view" dir="auto" aria-disabled="false" spellcheck="true">`;
+
+exports.tmplate_0 = tpl_pre;
+exports.tmplate_1 = tpl_next;
