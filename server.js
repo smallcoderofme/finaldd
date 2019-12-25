9397222a86a842ca28ace9e91ea14cb7a4e52700
smallcoderofme <svensama@outlook.com>
2019-12-25 12:42:35 +0800

    logic complete

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
+var FroalaEditor = require('./lib/froalaEditor');
 const uuidv4 = require('uuid/v4');
 const config = require('./config');
+
 const tpl = require('./template');
 
 app.use(express.static(__dirname+'/'));
@@ -14,94 +16,30 @@ app.use(express.static(__dirname+'/'));
 app.use(bodyParser.json(config.json_limit));
 app.use(bodyParser.urlencoded({ limit: config.json_limit, extended: false }));
 
-const BAST_PAGES_PATH = '/pages/';
-
 const admin_user = {username: 'admin', password:'hmcy123456'};
 var session = [];
 
-var data = [
-    {
-        "name":"科学馆篇",
-        "uuid":"35efe1dc-741a-4e67-a55d-ec9a848d9e1b",
-        "sub": [
-            {
-                "name":"生命科学馆介绍",
-                "uuid":"2257b8d4-a2f3-4a51-a1f0-75176bb46f1a",
-            },
-            {
-                "name":"生命之初",
-                "uuid":"4abc7321-9a0b-49c1-b9df-51f7279dcc71",
-            },
-            {
-                "name":"生命之美",
-                "uuid":"981989d7-2357-42ed-a3d6-a513557f114b",
-            },
-            {
-                "name":"生命之本",
-                "uuid":"ea52952c-b729-4c44-b1a3-cb1a375a7099",
-            }
-        ]
-    },
-    {
-        "name":"解剖学篇",
-        "uuid":"d3ebf8c9-89cf-4d09-bfa7-ef772bfb0e93",
-        "url":"anatomy",
-        "sub": [
-            {
-                "name":"解剖学名人名言",
-                "uuid":"62424643-541a-47cf-86a5-b1a76f00aca0",
-            },
-            {
-                "name":"解剖学发展历程",
-                "uuid":"cb4b1548-bbb5-403b-9610-497fe7632ed7",
-            },
-            {
-                "name":"解剖学人体九大系统",
-                "uuid":"66b17f54-e2dc-4517-a8c1-04ab01c9a6f0",
-            }
-        ]
-    },
-    {
-        "name":"人体生命篇",
-        "uuid":"13d3a4e8-e9c2-4ab5-bf5c-124ca688dc27",
-        "sub": [
-            {
-                "name":"大脑的奥义",
-                "uuid":"9e630e24-f1a7-4544-ab4d-ab788814548b",
-            },
-            {
-                "name":"基因工程与人类健康",
-                "uuid":"756108bf-33cc-4467-aa08-b4f32ae8544a",
-            },
-            {
-                "name":"DNA双螺旋结构的发现",
-                "uuid":"ed51c7ce-a96e-463e-ad63-4bae4f893168",
-            }
-        ]
-    },
-    {
-        "name":"医学院系篇",
-        "uuid":"4a73b2a5-670f-41f8-8f23-cba3c8839167",
-        "sub": [
-            {
-                "name":"学院风采",
-                "uuid":"ed51c7ce-a96e-463e-ad63-4bae4f893168",
-            },
-            {
-                "name":"院系介绍",
-                "uuid":"8205f62b-a9ad-4f75-8418-f606a0b12e79",
-            },
-            {
-                "name":"名师风采",
-                "uuid":"5e913398-f121-460b-92d8-93f422737a12",
-            },
-            {
-                "name":"实验室简介",
-                "uuid":"b20cc372-0041-4e10-94a2-26681c697a9e",
-            }
-        ]
-    }
-]
+var data_config;
+read_config();
+function read_config() {
+    fs.readFile('config.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        console.log('Read config file success!');
+        data_config = JSON.parse(data);
+    });
+}
+
+function write_config() {
+    fs.writeFile('config.json', JSON.stringify(data_config), (err) => {
+        if (err) {
+            throw err;
+        };
+        console.log('The config file has been saved!');
+    });
+}
+
 
 app.get('/', (req, res) => {
    res.sendFile(__dirname + '/src/index.html');
@@ -112,7 +50,7 @@ app.get('/menu', (req, res) => {
 });
 
 app.get('/list', (req, res) => {
-    res.send({status: 'ok', data: data});
+    res.send({status: 'ok', data: data_config});
 });
 
 app.get('/menu/:uuid', function(req, res) {
@@ -130,39 +68,39 @@ app.get('/admin', function (req, res) {
 
 app.post('/post_menu', function (req, res) {
     let new_menu = req.body.data;
-    // let tempObj = JSON.stringify({
-    //     name: new_menu,
-    //     uuid: uuidv4(),
-    //     sub: []
-    // });
-    data.push({
+    data_config.push({
         name: new_menu,
         uuid: uuidv4(),
         sub: []
     });
-    res.send({status: 'ok', data:data});
+    write_config();
+    res.send({status: 'ok', data:data_config});
+
+
 });
 app.post('/modify_menu', function (req, res) {
-    const len = data.length;
+    const len = data_config.length;
     const tempName = req.body.data;
     const id = req.body.uuid;
     for (let i =0;i<len;i++) {
-        if (data[i].uuid == id) {
-            data[i].name = tempName;
+        if (data_config[i].uuid == id) {
+            data_config[i].name = tempName;
         }
     }
-    res.send({status: 'ok', data:data});
+    write_config();
+    res.send({status: 'ok', data:data_config});
 });
 
 app.post('/delete_menu', function (req, res) {
-    const len = data.length;
-    const id = req.body.pid;
+    const len = data_config.length;
+    const id = req.body.uuid;
     for (let i =0;i<len;i++) {
-        if (data[i].uuid == id) {
-            data.splice(i,1);
+        if (data_config[i].uuid == id) {
+            data_config.splice(i,1);
         }
     }
-    res.send({status: 'ok', data:data});
+    write_config();
+    res.send({status: 'ok', data:data_config});
 });
 
 app.post('/auth/login', function (req, res) {
@@ -180,7 +118,7 @@ app.post('/auth/login', function (req, res) {
 });
 
 app.get('/edit', function(req, res) {
-    if(verify_token(req.query.token)){
+    if(req.query.token && verify_token(req.query.token)){
         res.sendFile(__dirname + '/src/menu_manage.html');
     } else {
         res.sendFile(__dirname + '/src/login.html');
@@ -198,69 +136,316 @@ app.get('/create_content', function (req, res) {
 
 app.post('/save_content', function(req, res) {
     const uuid = uuidv4();
-    const file_name = 'pages/'+ uuid + '.html';
+    const file_name = 'pages/' + uuid + '.html';
     const content_html = req.body.data;
-    // fs.readFile('config.json', 'utf8', (err, data) => {
-    //     if (err) {
-    //         res.send({
-    //             status: 'error',
-    //             data: "Error: config.js can't read or not exist."
-    //         });
-    //         throw err;
-    //     }
-        const len = data.length;
-        let parent;
-        console.log(req.body.pid, data);
-        for (let i=0;i<len;i++) {
-            if ( data[i].uuid == req.body.pid ) {
-                parent = data[i];
-                break;
-            }
+    const len = data_config.length;
+    let parent;
+    for (let i = 0; i < len; i++) {
+        if (data_config[i].uuid == req.body.pid) {
+            parent = data_config[i];
+            break;
         }
-        console.log('parent',parent);
-        parent.sub[parent.sub.length] = {
-            name: req.body.title,
-            uuid: uuid,
-        };
-
-    // });
-    fs.writeFile(file_name, tpl.tmplate_0+content_html+tpl.tmplate_1, (err) => {
+    }
+    parent.sub[parent.sub.length] = {
+        name: req.body.title,
+        uuid: uuid,
+    };
+    write_config();
+    fs.writeFile(file_name, tpl.tmplate_0 + content_html + tpl.tmplate_1, (err) => {
         if (err) throw err;
         console.log('The html file has been saved!');
-        res.send({status:'ok'});
+        res.send({status: 'ok'});
     });
 });
+app.post('/save_content/modify', function(req, res) {
+    const uuid = req.body.uuid;
+    const file_name = 'pages/' + uuid + '.html';
+    const content_html = req.body.data;
 
-
+    const len = data_config.length;
+    loop: for (let i = 0; i < len; i++) {
+        if (data_config[i].uuid == req.body.pid) {
+            let parent = data_config[i];
+            for (let sub of parent.sub) {
+                if (sub.uuid == uuid) {
+                    sub = {
+                        name: req.body.title,
+                        uuid: uuid
+                    }
+                    break loop;
+                }
+            }
+        }
+    }
+    write_config();
+    fs.writeFile(file_name, tpl.tmplate_0 + content_html + tpl.tmplate_1, (err) => {
+        if (err) throw err;
+        console.log('The html file has been saved!');
+        res.send({status: 'ok'});
+    });
+});
 app.get('/menu/:uuid/content/:cid', function(req, res) {
     // res.send({status:'ok', data:{pid: req.params.uuid, cid:req.params.cid}});
     res.sendFile(__dirname + '/pages/' + req.params.cid + '.html');
 });
 
+app.post('/delete/:pid/:cid', function (req, res) {
+    const pid = req.params.pid;
+    const cid = req.params.cid;
+
+    let len = data_config.length;
+    loop:for (let i=0;i<len;i++) {
+        let temp = data_config[i];
+        if (temp.uuid == pid) {
+            for (let j=temp.sub.length-1;j>-1;j--) {
+                if (temp.sub[j].uuid == cid) {
+                    temp.sub.splice(j,1);
+                    break loop;
+                }
+            }
+        }
+    }
+    res.send({status:'ok', data: data_config});
+});
+app.get('/modify', function (req, res) {
+    if(verify_token(req.query.token)){
+        res.sendFile(__dirname + '/src/edit.html');
+    } else {
+        res.sendFile(__dirname + '/src/login.html');
+    }
+});
+app.post('/modify', function (req, res) {
+    const pid = req.body.pid;
+    const cid = req.body.cid;
+    console.log('-------------',req.query.token);
+    if(req.query.token && !verify_token(req.query.token)){
+        res.send({status:'error', data: 'auth error'});
+    } else if(!req.query.token) {
+        res.send({status:'error', data: 'auth error'});
+    } else {
+        const file_name = 'pages/'+ cid + '.html';
+        console.log('pid:', pid, 'cid:',cid);
+        let name = '';
+        let len = data_config.length;
+
+        loop:for (let i=0;i<len;i++) {
+            let temp = data_config[i];
+            if (temp.uuid == pid) {
+                for (let j=temp.sub.length-1;j>-1;j--) {
+                    if (temp.sub[j].uuid == cid) {
+                        name = temp.sub[j].name;
+                        console.log('get name: ',name);
+                        break loop;
+                    }
+                }
+            }
+        }
+
+        fs.readFile(file_name, 'utf8', (err, content) => {
+            if (err) {
+                res.send({
+                    status: 'error',
+                    data: "Error: config.js can't read or not exist."
+                });
+                throw err;
+            }
+            const ctx_len = content.length - tpl.tpl_0_len - tpl.tpl_1_len;
+            const ctx = content.substr(tpl.tpl_0_len, ctx_len);
+
+            // console.log('name:',name);
+            res.send({status:'ok', data: ctx, name:name});
+        });
+    }
+});
+
+
+app.post('/upload_image', function (req, res) {
+
+    FroalaEditor.Image.upload(req, '/uploads/', function(err, data) {
+
+        if (err) {
+            return res.send(JSON.stringify(err));
+        }
+        res.send(data);
+    });
+});
+
+app.post('/upload_video', function (req, res) {
+
+    FroalaEditor.Video.upload(req, '/uploads/', function(err, data) {
+
+        if (err) {
+            return res.send(JSON.stringify(err));
+        }
+        res.send(data);
+    });
+});
+
+app.post('/upload_image_resize', function (req, res) {
+
+    var options = {
+        resize: [300, 300]
+    }
+    FroalaEditor.Image.upload(req, '/uploads/', options, function(err, data) {
+
+        if (err) {
+            return res.send(JSON.stringify(err));
+        }
+        res.send(data);
+    });
+});
+
+app.post('/upload_image_validation', function (req, res) {
+
+    var options = {
+        fieldname: 'myImage',
+        validation: function(filePath, mimetype, callback) {
+
+            gm(filePath).size(function(err, value){
+
+                if (err) {
+                    return callback(err);
+                }
+
+                if (!value) {
+                    return callback('Error occurred.');
+                }
+
+                if (value.width != value.height) {
+                    return callback(null, false);
+                }
+                return callback(null, true);
+            });
+        }
+    }
+
+    FroalaEditor.Image.upload(req, '/uploads/', options, function(err, data) {
+
+        if (err) {
+            return res.send(JSON.stringify(err));
+        }
+        res.send(data);
+    });
+});
+
+app.post('/upload_file', function (req, res) {
+
+    var options = {
+        validation: null
+    }
+
+    FroalaEditor.File.upload(req, '/uploads/', options, function(err, data) {
+
+        if (err) {
+            return res.status(404).end(JSON.stringify(err));
+        }
+        res.send(data);
+    });
+});
+
+app.post('/upload_file_validation', function (req, res) {
+
+    var options = {
+        fieldname: 'myFile',
+        validation: function(filePath, mimetype, callback) {
+
+            fs.stat(filePath, function(err, stat) {
+
+                if(err) {
+                    return callback(err);
+                }
+
+                if (stat.size > 10 * 1024 * 1024) { // > 10M
+                    return callback(null, false);
+                }
+
+                return callback(null, true);
+
+            });
+        }
+    }
+
+    FroalaEditor.File.upload(req, '/uploads/', options, function(err, data) {
+
+        if (err) {
+            return res.status(404).end(JSON.stringify(err));
+        }
+        res.send(data);
+    });
+});
+
+app.post('/delete_image', function (req, res) {
+
+    FroalaEditor.Image.delete(req.body.src, function(err) {
+
+        if (err) {
+            return res.status(404).end(JSON.stringify(err));
+        }
+        return res.end();
+    });
+});
+app.post('/delete_video', function (req, res) {
+
+    FroalaEditor.Video.delete(req.body.src, function(err) {
+
+        if (err) {
+            return res.status(404).end(JSON.stringify(err));
+        }
+        return res.end();
+    });
+});
+
+app.post('/delete_file', function (req, res) {
+
+    FroalaEditor.File.delete(req.body.src, function(err) {
+
+        if (err) {
+            return res.status(404).end(JSON.stringify(err));
+        }
+        return res.end();
+    });
+});
+
+app.get('/load_images', function (req, res) {
+
+    FroalaEditor.Image.list('/uploads/', function(err, data) {
+
+        if (err) {
+            return res.status(404).end(JSON.stringify(err));
+        }
+        return res.send(data);
+    });
+});
+
+
+app.get('/get_amazon', function (req, res) {
+
+    var configs = {
+        bucket: process.env.AWS_BUCKET,
+        region: process.env.AWS_REGION,
+        keyStart: process.env.AWS_KEY_START,
+        acl: process.env.AWS_ACL,
+        accessKey: process.env.AWS_ACCESS_KEY,
+        secretKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
+
+    var configsObj = FroalaEditor.S3.getHash(configs);
+    res.send(configsObj);
+});
+
+// Create folder for uploading files.
+var filesDir = path.join(path.dirname(require.main.filename), 'uploads');
+if (!fs.existsSync(filesDir)){
+    fs.mkdirSync(filesDir);
+}
+
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
 
-function pages_config_json_add(page_data) {
-    fs.readFile('page.config.json', 'utf8', (err, data) => {
-        if (err) {
-            res.sendFile(__dirname + '/src500.html');
-            console.log('page config error!');
-        }
-        var new_data = JSON.parse(data);
-        new_data[new_data.length] = {
-            name: page_data.title,
-            uuid: uuidv4(),
-        };
-        fs.writeFile('config.json', JSON.stringify(new_data), (err) => {
-            if (err) throw err;
-            console.log('The config file had been saved!');
-        });
-    });
-}
-
 function verify_token(token) {
-    for (let i = session.length - 1; i >= 0; i--) {
+    for (let i = session.length - 1; i >= -1; i--) {
         if(session[i].token == token) {
             return true;
         }
