c0be9071a2b3a40cea7c329ec29ce56111183ca8
smallcoderofme <svensama@outlook.com>
2019-12-31 16:19:00 +0800

    add 'Access-Control-Allow-Origin'

--- server.js
+++ server.js
@@ -46,6 +46,15 @@ function write_config() {
 //     res.sendFile(__dirname + '/test.html');
 // });
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 app.get('/', (req, res) => {
    res.sendFile(__dirname + '/src/index.html');
 });
